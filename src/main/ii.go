720ecdaa25b159732bbc1a0911ba03f58533ecbd
Fan <fanhaozh@38f9d3e5ca30.ant.amazon.com>
2019-08-05 00:48:00 +0800

    fix

--- src/main/ii.go
+++ src/main/ii.go
@@ -1,8 +1,12 @@
 package main
 
-import "os"
-import "fmt"
-import "mapreduce"
+import (
+	"os"
+  "fmt"
+	"mapreduce"
+	"sort"
+	"strings"
+)
 
 // The mapping function is called once for each piece of the input.
 // In this framework, the key is the name of the file that is being processed,
